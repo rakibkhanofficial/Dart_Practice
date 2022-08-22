3cb64c81811d38fc4494e88e5b1b35e66b9457d9
Rakib Khan <39366407+Rakib00111@users.noreply.github.com>
2022-08-22 23:47:47 +0600

    Add files via upload

---
+++ nestedifelse/bin/nestedifelse.dart
@@ -0,0 +1,22 @@
+import 'package:nestedifelse/nestedifelse.dart' as nestedifelse;
+
+void main() {
+  var a = 10;
+  var b = 20;
+  var c = 30;
+
+  if (a>b){
+    if(a>c){
+      print("a is greater");
+    }
+    else{
+      print("c is greater");
+    }
+  }
+  else if (b>c){
+    print("b is greater");
+  }
+  else {
+    print("c is greater");
+  }
+}  
---
+++ nestedifelse/test/nestedifelse_test.dart
@@ -0,0 +1,8 @@
+import 'package:nestedifelse/nestedifelse.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
