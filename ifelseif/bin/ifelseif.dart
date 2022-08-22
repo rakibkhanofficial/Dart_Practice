c3031fc7925e2a46c0589b5914afe8c340ef77a3
Rakib Khan <39366407+Rakib00111@users.noreply.github.com>
2022-08-22 23:46:44 +0600

    Add files via upload

---
+++ ifelseif/bin/ifelseif.dart
@@ -0,0 +1,21 @@
+import 'package:ifelseif/ifelseif.dart' as ifelseif;
+
+void main() {
+  var marks = 74;
+  if(marks > 85)
+  {
+    print("Excellent");
+  }
+  else if(marks>75)
+  {
+    print("Very Good");
+  }
+  else if(marks>65)
+  {
+    print("Good");
+  }
+  else
+  {
+    print("Average");
+  }
+}  
