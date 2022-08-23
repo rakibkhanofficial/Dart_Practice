3255ec546425d9885305dfa2ab87efd14b92d05c
Rakib Khan <khanrakib218@gmail.com>
2022-08-23 18:08:54 +0600

    Add files via upload

---
+++ dart_switch_case/bin/dart_switch_case.dart
@@ -0,0 +1,22 @@
+void main()
+{
+  // declaring a integer variable
+  int RollNum =  90014;
+
+// Evaluate the test-expression to find the match
+  switch (RollNum) {
+    case 90009:
+      print("My name is Joseph");
+      break;
+    case 90010:
+      print("My name is Peter");
+      break;
+    case 090011:
+      print("My name is Devansh");
+      break;
+
+// default block
+    default:
+      print("Roll number is not found");
+  }
+}
---
+++ dart_switch_case/test/dart_switch_case_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_switch_case/dart_switch_case.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
