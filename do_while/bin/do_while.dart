164dcc032f18785026213e463939ad54443d6fcf
Rakib Khan <khanrakib218@gmail.com>
2022-08-26 10:52:39 +0600

    Add files via upload

---
+++ do_while/bin/do_while.dart
@@ -0,0 +1,13 @@
+void main() {
+  int i = 10;
+  print("Dart do-while loop example");
+
+  do{
+
+
+    print(i);
+    i++;
+
+  }while(i<=20);
+  print("The loop is terminated");
+}
---
+++ do_while/test/do_while_test.dart
@@ -0,0 +1,8 @@
+import 'package:do_while/do_while.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
